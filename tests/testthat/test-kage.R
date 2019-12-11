6f696a6de58c52e6800f384cc233a443e91e3d30
nhk2120 <55127092+nhk2120@users.noreply.github.com>
2019-12-11 10:09:02 -0500

    dd

--- tests/testthat/test-kage.R
+++ tests/testthat/test-kage.R
@@ -1,9 +1,10 @@
-test_that("multiplication works", {
-  expect_equal(2 * 3, 6)
-})
+a <- kage("1996-05-28", "2019-10-17", "K")
+expect_equal(a, 24)
 
-a <- kage(1996-05-28, "2019-10-17", "K")
-expect_warning(a)
 
 b <- kage("1995-07-18", "2053-11-21", "G")
 expect_equal(b, 58)
+
+
+
+expect_error(kage("1996-05-28", "20133339-140-17", "G"))
